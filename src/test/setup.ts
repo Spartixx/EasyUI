7d103e2c0695e2f68248230de09c7ebfe6b6aa31
Nicolas <nicolasmacrez999@gmail.com>
2026-08-08 10:55:56 +0200

    test: fix vitest act error, unknown tests and form unhandled rejection

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,8 +1,16 @@
-import { beforeAll } from 'vitest'
+import { beforeAll, beforeEach } from 'vitest'
 import { setProjectAnnotations } from '@storybook/react'
 import * as previewAnnotations from '../../.storybook/preview'
 import './visualFonts.css'
 
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean
+}
+
 const project = setProjectAnnotations([previewAnnotations])
 
 beforeAll(project.beforeAll)
+
+beforeEach(function disableReactActEnvironment() {
+  globalThis.IS_REACT_ACT_ENVIRONMENT = false
+})
